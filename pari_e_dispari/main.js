1883a439fc755ecf31033c4890e84258b8527604
PaoloPascucci <pascuccip9@gmail.com>
2021-10-19 16:38:05 +0200

    Add pari e dispari

---
+++ pari_e_dispari/main.js
@@ -0,0 +1,34 @@
+
+/*L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+const User = prompt("Scegli pari o dispari");
+const UserNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+function getRandom() {
+    return Math.ceil(Math.random() * 5);
+}
+
+function summary() {
+    const Sum = UserNumber + getRandom()
+    console.log(Sum);
+}
+
+function Finalnumber(number) {
+    if (number % 2 == 0) {
+        return "pari";
+    }
+    else {
+        return "dispari";
+    }
+
+}
+
+if (User == Finalnumber()) {
+    document.getElementById('stamp').innerHTML = "Il giocatore ha vinto"
+}
+else {
+    document.getElementById('stamp').innerHTML = "Il giocatore ha perso"
+}
